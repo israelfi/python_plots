821a51c4f711c50bb13f54d6d8ecfbdbfadaf593
israel <israel.amaral@azureenacomcombr.onmicrosoft.com>
2022-04-28 10:59:59 -0300

    Adding new features to sine wave plot

--- sine_animation/README.md
+++ sine_animation/README.md
@@ -1,4 +1,4 @@
 # Sine Animation
 Plots a simple sine wave using `FuncAnimation()`
 ## Result
-<div style="text-align:center"><img src="./matplot003.gif" width="1050" height="525" /></div>
+<div style="text-align:center"><img src="./matplot003.gif" width="1050" height="787.5" /></div>
--- sine_animation/sine_animation.py
+++ sine_animation/sine_animation.py
@@ -1,27 +1,67 @@
 import numpy as np
-from matplotlib.animation import FuncAnimation
+from math import atan2
+import matplotlib.patches as patches
 from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-t = np.linspace(0, 2 * np.pi, 100)
-x = 5 * np.cos(t)
-y = 2 * np.sin(t)
-z = t
+t = np.linspace(0, 4 * np.pi, 200)
+amplitude = 2
+y = amplitude * np.sin(t)
+angles = np.zeros(t.shape[0])
+
+for k in range(t.shape[0] - 1):
+    angles[k] = atan2(y[k] - y[k - 1], t[k] - t[k - 1])
+
+rect_width = 0.4
+rect_height = 0.2
 
 
 def my_function(i):
     ax.cla()
-    plt.grid()
-    ax.set_xlim([0, t[-1]])
-    ax.set_ylim([np.min(y) - 0.1, np.max(y) + 0.1])
+    ax2.cla()
+    ax.grid()
+    ax2.grid()
+
     ax.plot(t[:i], y[:i])
+    rectangle_update = patches.Rectangle(xy=(t[i - 1] + (rect_height / 2 * np.sin(angles[i - 1])),
+                                             y[i - 1] - rect_height / 2 * np.cos(angles[i - 1])),
+                                         width=rect_width, height=rect_height, angle=np.rad2deg(angles[i - 1]),
+                                         color='green')
+
+    ax.set_title('Sine Wave')
+    ax.add_patch(rectangle_update)
+    ax.set_xlim([t[0], t[-1]])
+    ax.set_ylim([1.1 * min(y), 1.1 * max(y)])
+    ax.set_aspect('equal', adjustable='box')
+
+    ax2.set_title('Rectangle Angle')
+    ax2.plot(t[:i], angles[:i], c='orange', linewidth=2)
+    ax2.set_xlim([t[0], t[-1]])
+    ax2.set_ylim([1.1 * min(angles), 1.1 * max(angles)])
+    ax2.set_aspect('equal', adjustable='box')
+
+    plt.tight_layout()
 
 
-fig = plt.figure(figsize=(12, 6), facecolor='#DEDEDE')
-ax = plt.subplot()
+fig = plt.figure(figsize=(8, 6), facecolor='#DEDEDE')
+ax = fig.add_subplot(211)
 plt.grid()
-ax.set_xlim([0, t[-1]])
-ax.set_ylim([np.min(y) - 0.1, np.max(y) + 0.1])
+ax2 = fig.add_subplot(212)
+plt.grid()
 
-ani = FuncAnimation(fig, my_function, interval=200)
+rectangle = patches.Rectangle(xy=(0, 0), width=0.2, height=0.1, angle=np.rad2deg(angles[0]))
+ax.add_patch(rectangle)
+
+ax.set_title('Sine Wave')
+ax.set_xlim([t[0], t[-1]])
+ax.set_ylim([1.1 * min(y), 1.1 * max(y)])
+ax.set_aspect('equal', adjustable='box')
+
+ax2.set_title('Rectangle Angle')
+ax2.set_xlim([t[0], t[-1]])
+ax2.set_ylim([1.1 * min(angles), 1.1 * max(angles)])
+ax2.set_aspect('equal', adjustable='box')
+
+ani = FuncAnimation(fig, my_function, interval=50, frames=t.shape[0])
 # ani.save('matplot003.gif', writer='imagemagick')
 plt.show()
